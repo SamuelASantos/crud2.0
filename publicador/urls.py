aa40d21fe6ea32c7d8d6f1f56a41f8cb66192e30
Samuel Santos <54088727+SamuelASantos@users.noreply.github.com>
2023-12-30 21:19:49 -0300

    CREATE Complete
    Adicionado a segunda parte do CRUD [Create]

--- publicador/urls.py
+++ publicador/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path, include
-from .views import home
+from django.urls import path
+from .views import home, adicionar
 
 urlpatterns = [
     path('', home, name='home'),
+    path('adicionar/', adicionar, name='adicionar'),
 ]
--- publicador/views.py
+++ publicador/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Publicador
 
 # Create your views here.
 def home(request):
+    publicadores = Publicador.objects.all()
+    return render(request, 'index.html', {'publicadores':publicadores})
+
+def adicionar(request):
+    nome = request.POST.get('nome')
+    batismo = request.POST.get('batismo')
+    Publicador.objects.create(name=nome, baptism=batismo)
     publicadores = Publicador.objects.all()
     return render(request, 'index.html', {'publicadores':publicadores})
